ac345688e35f7ec193f0929845fd22da08e4ff7a
winwin2011 <yjh2011@live.com>
2021-09-21 11:27:43 +0800

    chore: update example

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { computed } from "@vue/reactivity";
+import { useTheme, useThemeController } from "../../";
+import { MyTheme } from "./main";
+const t = useTheme<MyTheme>();
+const controller = useThemeController();
+const setBlue = () => controller?.changeTheme("blue");
+const setRed = () => controller?.changeTheme("default");
+const h1Style = computed(() => ({ color: t?.value.primary }));
+const styles = computed(() => controller?.styles.value);
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div :style="styles">
+    <h1 :style="h1Style">Hello Vue 3 + @winwin/vue-global-theming!</h1>
+    <div>
+      <button @click="setBlue">chaneg to blue</button>
+      <button @click="setRed">chaneg to red</button>
+    </div>
+    <h2>CSS 变量：</h2>
+    <pre style="text-align: left">{{ styles }}</pre>
+  </div>
 </template>
 
 <style>
@@ -14,8 +29,16 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  background-color: var(--primary);
+  color: white;
+  padding: 5px 10px;
+  border: none;
+  margin-right: 10px;
+  cursor: pointer;
 }
 </style>
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createTheme } from "../..";
 
-createApp(App).mount('#app')
+export type MyTheme = {
+  primary: string;
+};
+
+const redTheme: MyTheme = {
+  primary: "#e74c3c",
+};
+const blueTheme: MyTheme = {
+  primary: "#3498db",
+};
+
+const theme = createTheme({
+  default: redTheme,
+  blue: blueTheme,
+});
+
+createApp(App).use(theme).mount("#app");
